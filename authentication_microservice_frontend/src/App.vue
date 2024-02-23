3be0bd2482f38093a8064c62f746984858bf4f67
vaanskii <giorgivanadze03@gmail.com>
2024-02-23 12:24:23 +0400

    Created Login page.

--- authentication_microservice_frontend/src/App.vue
+++ authentication_microservice_frontend/src/App.vue
@@ -1,5 +1,30 @@
 <template>
-
+  <nav class="bg-gray-900">
+    <router-link to="/signup">Signup</router-link> |
+    <router-link to="/login">Login</router-link>
+  </nav>
   <router-view/>
 </template>
 
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- authentication_microservice_frontend/src/router/index.js
+++ authentication_microservice_frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SignupView from '@/views/SignupView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const routes = [
   {
@@ -14,13 +15,10 @@ const routes = [
     component: SignupView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
 ]
 
 const router = createRouter({
