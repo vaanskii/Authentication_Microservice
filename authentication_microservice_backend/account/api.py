dd2b3f90ac15a5403d07fda88a8b111dd8ff7cdb
vaanskii <giorgivanadze03@gmail.com>
2024-02-23 11:44:16 +0400

    Integrate account. - Integrate account where user can create account or login.

---
+++ authentication_microservice_backend/account/api.py
@@ -0,0 +1,36 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
+from .models import User
+from .forms import SignupForm
+
+@api_view(['GET'])
+def me(request):
+    return JsonResponse({
+        'id': request.user.id,
+        'username': request.user.username,
+        'email': request.user.email,
+    })
+
+@api_view(['POST'])
+@authentication_classes([])
+@permission_classes([])
+def signup(request):
+    data = request.data
+    message = 'success'
+
+    form = SignupForm({
+        'email': data.get('email'),
+        'username': data.get('username'),
+        'password1': data.get('password1'),
+        'password2': data.get('password2'),
+    })
+
+    if form.is_valid():
+        user = form.save()
+        user.save()
+    
+    else:
+        message = form.errors.as_json()
+
+    return JsonResponse({'message': message}, safe=False)
